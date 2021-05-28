414629687ad16df964b5efaeb58ad01d7c277fee
pbteja100 <pbteja100@gmail.com>
2021-05-28 14:30:12 +0530

    garlic: Make Symlinks to FP firmware in /firmware from /vendor
    Also hexedit goodix and elan blobs to load look for firmware in /vendor/etc/firmware instead of /system
    
    Change-Id: I1330a1a877ad32c94a2df6e408b1fd77e2228d32

--- Android.mk
+++ Android.mk
@@ -18,4 +18,30 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),garlic)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+ELANFP_IMAGES := \
+    elanfp.b00 elanfp.b01 elanfp.b02 elanfp.b03 elanfp.b04 elanfp.b05 \
+    elanfp.b06 elanfp.mdt
+
+ELANFP_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_ETC)/firmware/,$(notdir $(ELANFP_IMAGES)))
+$(ELANFP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "ELANFP firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(ELANFP_SYMLINKS)
+
+GOODIXFP_IMAGES := \
+    goodixfp.b00 goodixfp.b01 goodixfp.b02 goodixfp.b03 goodixfp.b04 goodixfp.b05 \
+    goodixfp.b06 goodixfp.mdt
+
+GOODIXFP_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_ETC)/firmware/,$(notdir $(GOODIXFP_IMAGES)))
+$(GOODIXFP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "GOODIXFP firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GOODIXFP_SYMLINKS)
 endif
