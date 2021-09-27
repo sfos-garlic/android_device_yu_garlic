c76fb4465cab60b54391e90850bfd6c11b2ad525
pbteja100 <pbteja100@gmail.com>
2021-09-27 10:26:52 +0530

    garlic: Fstab is defined in tinno msm8937-common
    Change-Id: I1040f61e767ba51037a12b05a83979911d26d487

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,9 +1,10 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.qcom
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := fstab.qcom
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
+## Already Defined in tinno-common
+#LOCAL_PATH := $(call my-dir)
+#
+#include $(CLEAR_VARS)
+#LOCAL_MODULE       := fstab.qcom
+#LOCAL_MODULE_TAGS  := optional
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_SRC_FILES    := fstab.qcom
+#LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+#include $(BUILD_PREBUILT)
